f9397b1208d30a64533852e469d8d9802e19b33c
Mona <moniahmad@hotmail.com>
2026-04-29 11:27:59 +0200

    ändrat i main

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,20 @@ import controller.GameController;
 public class Main {
   public static void main(String[] args) {
     Board board = new Board();
-    GameModel gm = new GameModel(board,5);
+
+    GameModel gm = new GameModel(board, 5);
+
     GameController controller = new GameController(gm);
-    TerminalView terminalView = new TerminalView(gm);
+
+    TerminalView terminalView =
+        new TerminalView(gm, controller);
 
     gm.addObserver(terminalView);
 
     board.randomizeBoard();
+
+    gm.notifyObservers();
+
     terminalView.run();
   }
 }
-

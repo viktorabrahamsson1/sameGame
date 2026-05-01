eae97f15d03d4c5a3f66cc9441f6817bd29ef873
Viktor <vikabr0524@gmail.com>
2026-05-01 12:50:37 +0200

    added some test files

---
+++ test/controller/GameControllerTest.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameControllerTest {
+
+  @Test
+  void playMove() {
+  }
+
+  @Test
+  void startNewGame() {
+  }
+}

8c6aae64d6876132127b14fb18b5b06263e283fc
Viktor <vikabr0524@gmail.com>
2026-05-05 13:37:50 +0200

    added javadocs for moveSuggestion.java

--- src/model/MoveSuggestion.java
+++ src/model/MoveSuggestion.java
@@ -1,12 +1,21 @@
 package model;
 
 
+/**
+ * contains information about the best suggested move.
+ */
 public class MoveSuggestion {
   private final int row;
   private final int col;
   private final int groupSize;
   private final int points;
 
+  /**
+   *
+   * @param row the row index of a tile
+   * @param col the col index of a tile
+   * @param groupSize the size of connected tiles from row and col indexes
+   */
   public MoveSuggestion(int row, int col, int groupSize) {
     this.row = row;
     this.col = col;
